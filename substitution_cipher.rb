6df5d6f730f9d39980467f5e48d42ad05430338e
Cheng-Yu Hsu <crazyming411@gmail.com>
2015-03-30 16:08:16 +0800

    Make `substitution_cipher.rb` more readable.

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -8,7 +8,7 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       document.to_s.chars.map do |char|
-        ((char.ord + key) % 128).chr
+        ((char.ord + key) % ORD_RANGE.length).chr
       end.join
     end
 
@@ -19,7 +19,7 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       document.to_s.chars.map do |char|
-        ((char.ord - key) % 128).chr
+        ((char.ord - key) % ORD_RANGE.length).chr
       end.join
     end
   end
