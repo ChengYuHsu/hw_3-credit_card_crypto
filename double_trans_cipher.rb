fe95dba89c6961a84fcd7f736c339b9fb12ea7c7
Cheng-Yu Hsu <crazyming411@gmail.com>
2015-03-31 19:52:01 +0800

    Double trans cipher refatoring done.

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,5 +1,6 @@
 # Double transposition cipher
 module DoubleTranspositionCipher
+
   def self.gen_mapping(doc_len, factor, key, reversed = false)
     num_gen = Random.new(key.to_i)
     mapping = (0..doc_len / factor - 1).to_a.shuffle(random: num_gen).product((0..factor - 1).to_a.shuffle(random: num_gen)).map { |pair| pair.first * factor + pair.last }
@@ -8,36 +9,24 @@ module DoubleTranspositionCipher
   end
 
   def self.encrypt(document, key)
-    int_key = key.to_i
-    doc_len = document.to_s.length
-    ((doc_len**0.5).to_i..doc_len).each do |num|
-      if doc_len % num == 0
-        rng = Random.new(key.to_i)
-        row_order = (0..doc_len/num-1).to_a.shuffle(random: rng)
-        col_order = (0..num-1).to_a.shuffle(random: rng)
-        mapping = row_order.product(col_order).map { |pair| pair.first * num + pair.last}
-        doc_chars = document.to_s.chars
-        return doc_chars.each_with_index.map do |char, index|
-          doc_chars[mapping[index]]
-        end.join
-      end
+    doc_str = document.to_s
+    ((doc_str.length**0.5).to_i..doc_str.length).each do |num|
+      next if doc_str.length % num != 0
+      mapping = self.gen_mapping(doc_str.length, num, key)
+      return doc_str.chars.each_with_index.map do |dummy, index|
+        doc_str[mapping[index]]
+      end.join
     end
   end
 
   def self.decrypt(ciphertext, key)
-    int_key = key.to_i
-    doc_len = ciphertext.to_s.length
-    ((doc_len**0.5).to_i..doc_len).each do |num|
-      if doc_len % num == 0
-        rng = Random.new(key.to_i)
-        row_order = (0..doc_len/num-1).to_a.shuffle(random: rng)
-        col_order = (0..num-1).to_a.shuffle(random: rng)
-        mapping = Hash[row_order.product(col_order).map { |pair| pair.first * num + pair.last}.zip((0..doc_len-1).to_a)]
-        doc_chars = ciphertext.to_s.chars
-        return doc_chars.each_with_index.map do |char, index|
-          doc_chars[mapping[index]]
-        end.join
-      end
+    cip_str = ciphertext.to_s
+    ((cip_str.length**0.5).to_i..cip_str.length).each do |num|
+      next if cip_str.length % num != 0
+      mapping = self.gen_mapping(cip_str.length, num, key, reversed = true)
+      return cip_str.chars.each_with_index.map do |dummy, index|
+        cip_str[mapping[index]]
+      end.join
     end
   end
 end
