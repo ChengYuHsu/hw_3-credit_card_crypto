736690c4be37cae4f2889559d62f68c0cecde816
Cheng-Yu Hsu <crazyming411@gmail.com>
2015-03-31 19:59:36 +0800

    Refactoring double trans cipher.

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,19 +1,17 @@
 # Double transposition cipher
 module DoubleTranspositionCipher
-  
   def self.gen_mapping(doc_len, factor, key, reversed = false)
     num_gen = Random.new(key.to_i)
     mapping = (0..doc_len / factor - 1).to_a.shuffle(random: num_gen).product((0..factor - 1).to_a.shuffle(random: num_gen)).map { |pair| pair.first * factor + pair.last }
     mapping = Hash[mapping.zip((0..doc_len - 1).to_a)] if reversed
     mapping
   end
-  
 
   def self.encrypt(document, key)
     doc_str = document.to_s
     ((doc_str.length**0.5).to_i..doc_str.length).each do |num|
       next if doc_str.length % num != 0
-      mapping = self.gen_mapping(doc_str.length, num, key)
+      mapping = gen_mapping(doc_str.length, num, key)
       return doc_str.chars.each_with_index.map do |_, index|
         doc_str[mapping[index]]
       end.join
@@ -24,7 +22,7 @@ module DoubleTranspositionCipher
     cip_str = ciphertext.to_s
     ((cip_str.length**0.5).to_i..cip_str.length).each do |num|
       next if cip_str.length % num != 0
-      mapping = self.gen_mapping(cip_str.length, num, key, true)
+      mapping = gen_mapping(cip_str.length, num, key, true)
       return cip_str.chars.each_with_index.map do |_, index|
         cip_str[mapping[index]]
       end.join
