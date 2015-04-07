0282c36604ebca460484cb23438d8d74329d1bbb
Cheng-Yu Hsu <crazyming411@gmail.com>
2015-04-07 12:03:02 +0800

    Refactor `double_trans_cipher.rb`:
    - extract repeated code to methods

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,32 +1,34 @@
 # Double transposition cipher
 module DoubleTranspositionCipher
+  def self.gen_divisor(doc_len)
+    (doc_len**0.5).to_i.upto(doc_len).each do |num|
+      next if doc_len % num != 0
+      return num
+    end
+  end
 
-  def self.gen_mapping(doc_len, factor, key, reversed = false)
+  def self.gen_mapping(doc_len, divisor, key, reversed = false)
     num_gen = Random.new(key.to_i)
-    mapping = (0..doc_len / factor - 1).to_a.shuffle(random: num_gen).product((0..factor - 1).to_a.shuffle(random: num_gen)).map { |pair| pair.first * factor + pair.last }
-    mapping = Hash[mapping.zip((0..doc_len - 1).to_a)] if reversed
+    mapping = (0..doc_len / divisor - 1).to_a.shuffle(random: num_gen)
+              .product((0..divisor - 1).to_a.shuffle(random: num_gen))
+              .map { |pair| pair.first * divisor + pair.last }
+    mapping = Hash[mapping.zip(0..doc_len - 1)] if reversed
     mapping
   end
 
   def self.encrypt(document, key)
-    doc_str = document.to_s
-    ((doc_str.length**0.5).to_i..doc_str.length).each do |num|
-      next if doc_str.length % num != 0
-      mapping = gen_mapping(doc_str.length, num, key)
-      return doc_str.chars.each_with_index.map do |_, index|
-        doc_str[mapping[index]]
-      end.join
-    end
+    str = document.to_s
+    mapping = gen_mapping(str.length, gen_divisor(str.length), key)
+    str.chars.each_with_index.map do |_, index|
+      str[mapping[index]]
+    end.join
   end
 
   def self.decrypt(ciphertext, key)
-    cip_str = ciphertext.to_s
-    ((cip_str.length**0.5).to_i..cip_str.length).each do |num|
-      next if cip_str.length % num != 0
-      mapping = gen_mapping(cip_str.length, num, key, true)
-      return cip_str.chars.each_with_index.map do |_, index|
-        cip_str[mapping[index]]
-      end.join
-    end
+    str = ciphertext.to_s
+    mapping = gen_mapping(str.length, gen_divisor(str.length), key, true)
+    str.chars.each_with_index.map do |_, index|
+      str[mapping[index]]
+    end.join
   end
 end
