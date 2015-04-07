40ce8e38fe99b8849c176bcd17c0f41e7e7204a4
Cheng-Yu Hsu <crazyming411@gmail.com>
2015-04-07 12:33:20 +0800

    Refactor `spec/crypto_spec.rb`: add more tests.

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -1,33 +1,47 @@
 require_relative '../credit_card'
 require_relative '../substitution_cipher'
 require_relative '../double_trans_cipher'
+require_relative '../aes_cipher.rb'
 require 'minitest/autorun'
+require 'securerandom'
 
-# put your crypto module here
+# put your crypto modules here
 cryptos = [
-  SubstitutionCipher::Caesar,
-  SubstitutionCipher::Permutation,
-  DoubleTranspositionCipher
+  {
+    method: SubstitutionCipher::Caesar,
+    key: Random.rand(20)
+  },
+  {
+    method: SubstitutionCipher::Permutation,
+    key: Random.rand(20)
+  },
+  {
+    method: DoubleTranspositionCipher,
+    key: Random.rand(20)
+  },
+  {
+    method: AesCipher,
+    key: SecureRandom.random_bytes([128, 192, 256].sample / 8)
+  }
 ]
 
 describe 'Test card info encryption' do
   # before each testing
   before do
     @cc = CreditCard.new('4916603231464963', 'Mar-30-2020', 'Soumya Ray', 'Visa')
-    @key = 3
   end
   # test all the crypto methods
-  cryptos.each do |method|
+  cryptos.each do |crypto|
     describe 'Using #{method}' do
       # test encryption
       it 'should encrypt card information' do
-        enc = method.encrypt(@cc, @key)
+        enc = crypto[:method].encrypt(@cc, crypto[:key])
         enc.wont_equal @cc.to_s
       end
       # test decryption
       it 'should decrypt text' do
-        enc = method.encrypt(@cc, @key)
-        dec = method.decrypt(enc, @key)
+        enc = crypto[:method].encrypt(@cc, crypto[:key])
+        dec = crypto[:method].decrypt(enc, crypto[:key])
         dec.must_equal @cc.to_s
       end
     end
